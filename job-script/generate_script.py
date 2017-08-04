57a9bf2d7ee80046e9fbaa4c7c3702c9a2cdfa04
Ari Pratama Zhorifiandi <arizho16@gmail.com>
2017-08-05 00:18:12 +0700

    gen script

--- job-script/generate_script.py
+++ job-script/generate_script.py
@@ -8,4 +8,4 @@ for i in range (0,50):
 	file_string = "echo Start Time of writing jar file: $(date)\n"
 	file_string += hadoop_path + " jar " + workgen_path + " " + workgen_class + " -conf " + conf_path + " -r 1 inputPath-job-"+str(i)+".txt workGenOutputTest-"+str(i)+" 5.810261E-4 0.26818323 >> workGenLogs/job-"+str(i)+".txt 2>> workGenLogs/job-"+str(i)+".txt \n"
 	file_string += hadoop_path + " dfs -rmr workGenOutputTest-"+str(i)+"\n"
-	print(file_string, file=open("scriptsTest/run-job-"+str(i)+".txt","w"))
+	print(file_string, file=open("scriptsTest/run-job-"+str(i)+".sh","w"))
